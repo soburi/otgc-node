06ea006a57dcd8cfba3cf67baed6df9aa535547e
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-11-21 16:03:01 +0900

    tmp

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app" class="container">
+  <div id="app" class="container-fluid container-root">
+    <div id="wrap" class="wrap bg-light align-items-center justify-content-center flex-fill">
       <b-navbar toggleable="lg" type="dark" variant="info">
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item href="#">Discover</b-nav-item>
@@ -15,30 +15,108 @@
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
+      <div id="main" class="row">
+<!--
+        <div id="xmain" class="col devicelist">
+          <b-list-group>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          <b-list-group-item button>This is a button too</b-list-group-item>
+          </b-list-group>
+	</div>
+-->
+        <div id="xmain" class="h-auto">
+          <b-list-group id="propertylist" class="h-50 overflow-auto">
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+            <b-list-group-item button>Button item</b-list-group-item>
+          </b-list-group>
 
-    <div id="main" class="row">
-    <div class="col">
-      <b-list-group>
-        <b-list-group-item button>Button item</b-list-group-item>
-        <b-list-group-item button>I am a button</b-list-group-item>
-        <b-list-group-item button disabled>Disabled button</b-list-group-item>
-        <b-list-group-item button>This is a button too</b-list-group-item>
-      </b-list-group>
-    </div>
+          <b-tabs row class="h-50">
+            <b-tab title="Generic Client"><p>I'm the first tab</p></b-tab>
+            <b-tab title="Access Control"><p>I'm the second tab</p></b-tab>
+            <b-tab title="Credential"><p>I'm a disabled tab!</p></b-tab>
+            <b-tab title="Linked Devices"><p>I'm a disabled tab!</p></b-tab>
+          </b-tabs>
+        </div>
 
-    <div class="col">
-      <b-list-group id="propertylist">
-        <b-list-group-item button>Button item</b-list-group-item>
-      </b-list-group>
-      <b-tabs content-class="mt-3">
-        <b-tab title="Generic Client"><p>I'm the first tab</p></b-tab>
-        <b-tab title="Access Control"><p>I'm the second tab</p></b-tab>
-        <b-tab title="Credential"><p>I'm a disabled tab!</p></b-tab>
-        <b-tab title="Linked Devices"><p>I'm a disabled tab!</p></b-tab>
-      </b-tabs>
+      </div>
     </div>
-    </div>
-
   </div>
 </template>
 
+<style scoped>
+
+::webkit-scrollbar {
+    display: none;
+}
+
+.container-root {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+.devicelist {
+    overflow: scroll;
+    height: 100%;
+}
+
+.propertylist {
+    overflow: scroll;
+    height: 100%;
+}
+
+.wrap { 
+    margin-right: -15px; 
+    margin-left: -15px; 
+} 
+
+.full-height {
+  height: 100vh;
+  width: 100vw;
+}
+
+.flex-fill {
+    flex: 1 1 auto;
+}
+</style>
